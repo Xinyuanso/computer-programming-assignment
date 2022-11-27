fd2045b29382c4b7e3ef41d241001df9650a44eb
Xinyuanso <51281832+Xinyuanso@users.noreply.github.com>
2022-11-27 21:40:02 +0800

    Add files via upload

---
+++ ui.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include<string.h>
+#include<math.h>
+
+
+using namespace std;
+
+int main(){
+
+char pass[10];
+int i;
+
+cout<<"                 _______ _______ ______   ___ __    _ _______      _______ _______ _______ ___   _ _______            "<<endl;
+cout<<"                |       |       |      |_|   |  |  | |       |    |       |       |       |   | | |       |           "<<endl;
+cout<<"                |   ____|   _   |  _     |   |   |_| |    ___|    |    ___|    ___|    ___|   |_| |  _____|           "<<endl;
+cout<<"                |  |       | |  | | |    |   |       |   | __     |   | __|   |___|   |___|      _| |_____            "<<endl;
+cout<<"                |  |____   |_|  | |_|    |   |  _    |   ||  |    |   ||  |    ___|    ___|     |_|_____  |           "<<endl;
+cout<<"                |       |       |       _|   | | |   |   |_| |    |   |_| |   |___|   |___|    _  |_____| |           "<<endl;
+cout<<"                |_______|_______|______| |___|_|  |__|_______|    |_______|_______|_______|___| |_|_______|           "<<endl;
+
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"========================================================================================================================"<<endl;
+cout<<" "<<endl;
+cout<<"                               WELCOME TO CODING GEEKS STORE MANAGEMENT SYSTEM !                                        "<<endl;
+cout<<" "<<endl;
+cout<<"========================================================================================================================"<<endl;
+
+cout<<"\n\n\n\n\n\n\n\t\t\t\t\tPlease Enter Your Password: ";
+cin>> pass;
+
+
+
+if(strcmp(pass,"codinggeeks")==0)
+	{
+
+    system("cls");
+cout<< "Please continue..."<<endl;
+
+
+
+}
+
+else
+{
+	cout<<"Wrong Password,Exit Now...";
+
+}
+	}
