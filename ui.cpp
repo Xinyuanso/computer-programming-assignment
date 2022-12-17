536e5fb8f241e63ea6ea3a33f54875f21e1cbb56
Junda10 <119513588+Junda10@users.noreply.github.com>
2022-12-17 18:19:19 +0800

    Update ui.cpp

--- ui.cpp
+++ ui.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void logo(int e);
+void menu();
+void supplierdetails();
+void managestock();
+void exit();
+void managestock();
+void displaystock();
+
+void exit()
+    {
+    cout << "\n\nTHANKS FOR USING CODING GEEKS STORE MANAGEMENT SYSTEM"<<endl;
+    }
+
 void logo(int e)
 {
+    do{
 cout<<"                 _______ _______ ______   ___ __    _ _______      _______ _______ _______ ___   _ _______            "<<endl;
 cout<<"                |       |       |      |_|   |  |  | |       |    |       |       |       |   | | |       |           "<<endl;
 cout<<"                |   ____|   _   |  _     |   |   |_| |    ___|    |    ___|    ___|    ___|   |_| |  _____|           "<<endl;
@@ -23,32 +37,71 @@ cout<<"\n\n\n\n\n\n\n\t\t\t\t\t Press 1 to continue. "<< endl;
 cin>> e;
 
 system("cls");
+}while (e != 1);
 }
 
-void menu(int choice_1){
-    cout<<"\n\n\t\t   1. To display products\n\n\t\t   2. To Manage product quantity\n\n\t\t      3. Supplier Details\n\n\t\t ";
-	cout<<"\n\n=============================================================\n";
+void menu()
+{
+    int choice_1;
+    int input;
+    cout<<"========================================================================================================================"<<endl;
+    cout<<" "<<endl;
+    cout<<"                               CODING GEEKS STORE MANAGEMENT SYSTEM MENU                                       "<<endl;
+    cout<<" "<<endl;
+    cout<<"========================================================================================================================"<<endl;
+    cout<<"\n\n\t\t 1. To add new products\n\n      ";
+    cout<<"\t\t 2. Supplier Details\n\n ";
+    cout<<"\t\t 3. Exit\n\n ";
+	cout<<"\n\n========================================================================================================================\n";
 	cout<<"\n\nEnter Your Choice:";
 	cin>>choice_1;
-    system("cls");
+
+	switch(choice_1)
+	{
+    case 1:
+        system("cls");
+        managestock();
+        break;
+    case 2:
+        system("cls");
+        supplierdetails();
+        break;
+    case 3:
+        system("cls");
+        exit();
+        break;
+    default:
+        cout << "Invalid input.Please enter again : " << endl;
+        menu();
+        break;
+	}
 }
-
-void logo(int e);
-void menu(int choice_1);
-
 
 int main(){
     int e;
     int choice_1;
-    logo(e);
-    menu(choice_1);
-
     const int model_num=3;
     const int stock_num=2;
-    int addstock[model_num][stock_num];
-    int * p1;
-    p1 = &addstock[model_num][stock_num];
-    string response;
+    int addstock[model_num][stock_num],t;
+    string stock[model_num]={"Pen","Eraser","Ruler"};
+    string model[stock_num]={"A","B"};
+    string response,ds;
+    logo(e);
+    menu();
+    return 0;
+}
+
+void supplierdetails()
+{
+    cout << "In progress" << endl;
+    return ;
+}
+
+void managestock(){
+    const int model_num=3;
+    const int stock_num=2;
+    int addstock[model_num][stock_num],t;
+    string response,ds;
 
     do{
     string stock[model_num]={"Pen","Eraser","Ruler"};
@@ -58,26 +111,41 @@ int main(){
     {
         for(int j=0;j<2;j++)
         {
-
     cout << "Please add stock for "<< stock[i]<< " model "<< model[j] << ":" ;
     cin >> addstock[i][j];
-
-
+    if (addstock[i][j] < 0  )
+        {
+            cout << "Please enter the correct product number :" << endl;
+            cin >> addstock[i][j];
         }
-
+        }
     }
-
+    cout << "\nDo u want to display the stock avialable (yes/no) ? :";
+    cin >> ds;
+    if (ds == "yes")
+    {
+    cout << "\n\n----------Stock Available----------" << endl;
     for (int i=0;i<3;i++)
     {
         for(int j=0;j<2;j++)
         {
-            cout << "\nThe stock for "<< stock[i]<< " model " <<model[j]<<" is " << *p1 <<endl;
+            cout << "\nThe stock for "<< stock[i]<< " model " <<model[j]<<" is " << addstock[i][j] ;
         }
     }
-
-    cout << "Do u want to continue add stock (yes/no) : " ;
+    }else if (ds == "no")
+    {
+            cout << "\n\n\n-------------------------------------------------\n";
+            cout << "\nThank you for add new product." << endl;
+            system("cls");
+            menu();
+    }else if (ds != "yes" || ds != "no")
+    {
+        cout << "Invalid answer please redo!!!"<<endl;
+        managestock();
+    }
+    cout << "\n\nDo u want to change the quantity of product (yes/no) : " ;
     cin >> response;
-        while ( response != "yes" && response != "no" )
+       while ( response != "yes" && response != "no" )
         {
             cout << "Incorrect responce." << endl;
             cout << "Please re-enter your answer (yes/no): ";
@@ -86,15 +154,12 @@ int main(){
 
         if ( response == "no" )
         {
-            cout << "\n\n\n-------------------------------------------------\n";
-            cout << "\nThank you for add stock." << endl;
+            cout << "\n\n-------------------------------------------------\n";
+            cout << "\nThank you for add new product." << endl;
+            menu();
         }
-    }while (response == "yes"); // have bug in this code cause cannot display the sum of stock
-	//have try to use * and & to get the number but fail.
-	//if can slove this then we use this loop which answer=yes will continue update
-	//else delete;
+    }while (response == "yes");
 }
-
 
 void pen()
 {
@@ -113,8 +178,5 @@ void ruler()
     cout<<"Ruler model A:       15cm\n";
     cout<<"Ruler model B:       30cm\n";
 }
-
-
-
 
 
