d64a3e4aa61a207097d3f13befa9eedb723b3809
Junda10 <119513588+Junda10@users.noreply.github.com>
2022-12-17 10:21:49 +0800

    Update ui.cpp

--- ui.cpp
+++ ui.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include<string.h>
-#include<math.h>
-
-
 using namespace std;
 
-int main(){
-
-char pass[10];
-int i;
-
+void logo(int e)
+{
 cout<<"                 _______ _______ ______   ___ __    _ _______      _______ _______ _______ ___   _ _______            "<<endl;
 cout<<"                |       |       |      |_|   |  |  | |       |    |       |       |       |   | | |       |           "<<endl;
 cout<<"                |   ____|   _   |  _     |   |   |_| |    ___|    |    ___|    ___|    ___|   |_| |  _____|           "<<endl;
@@ -26,24 +19,99 @@ cout<<"                               WELCOME TO CODING GEEKS STORE MANAGEMENT S
 cout<<" "<<endl;
 cout<<"========================================================================================================================"<<endl;
 
-cout<<"\n\n\n\n\n\n\n\t\t\t\t\tPlease Enter Your Password: ";
-cin>> pass;
+cout<<"\n\n\n\n\n\n\n\t\t\t\t\t Press 1 to continue. "<< endl;
+cin>> e;
 
+system("cls");
+}
 
-
-if(strcmp(pass,"codinggeeks")==0)
-	{
-
+void menu(int choice_1){
+    cout<<"\n\n\t\t   1. To display products\n\n\t\t   2. To Manage product quantity\n\n\t\t      3. Supplier Details\n\n\t\t ";
+	cout<<"\n\n=============================================================\n";
+	cout<<"\n\nEnter Your Choice:";
+	cin>>choice_1;
     system("cls");
-cout<< "Please continue..."<<endl;
-
-
-
 }
 
-else
+void logo(int e);
+void menu(int choice_1);
+
+
+int main(){
+    int e;
+    int choice_1;
+    logo(e);
+    menu(choice_1);
+
+    const int model_num=3;
+    const int stock_num=2;
+    int addstock[model_num][stock_num];
+    int * p1;
+    p1 = &addstock[model_num][stock_num];
+    string response;
+
+    do{
+    string stock[model_num]={"Pen","Eraser","Ruler"};
+    string model[stock_num]={"A","B"};
+
+    for (int i=0;i<3;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+
+    cout << "Please add stock for "<< stock[i]<< " model "<< model[j] << ":" ;
+    cin >> addstock[i][j];
+
+
+        }
+
+    }
+
+    for (int i=0;i<3;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            cout << "\nThe stock for "<< stock[i]<< " model " <<model[j]<<" is " << *p1 <<endl;
+        }
+    }
+
+    cout << "Do u want to continue add stock (yes/no) : " ;
+    cin >> response;
+        while ( response != "yes" && response != "no" )
+        {
+            cout << "Incorrect responce." << endl;
+            cout << "Please re-enter your answer (yes/no): ";
+            cin >> response;
+        }
+
+        if ( response == "no" )
+        {
+            cout << "\n\n\n-------------------------------------------------\n";
+            cout << "\nThank you for add stock." << endl;
+        }
+    }while (response == "yes");
+}
+
+
+void pen()
 {
-	cout<<"Wrong Password,Exit Now...";
-
+    cout<<"Pen model A:         0.5\n";
+    cout<<"Pen model B:         0.7\n";
 }
-	}
+
+void eraser()
+{
+    cout<<"Eraser model A: 2 x 3 cm\n";
+    cout<<"Eraser model B: 4 x 6 cm\n";
+}
+
+void ruler()
+{
+    cout<<"Ruler model A:       15cm\n";
+    cout<<"Ruler model B:       30cm\n";
+}
+
+
+
+
+
